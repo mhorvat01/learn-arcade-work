4353ed45c5a7266e8313472ca68704ba10617397
MATTEO <mhorvat01@overlake.org>
2021-09-09 13:25:12 -0700

    commit

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,6 @@
+print (""" Hello to the reader of this, today I will tell you a story
+the story starts off with Joe, Joe walked down the road.
+Joe saw a big train, Joe decided to get on the train.
+The train was moving so he had to run, he ran as fast as he could.
+Eventually he grabbed a rail and pulled himself into the train.
+Ten days later the train stopped in Italy and he got out, he was now living in Italy""")
